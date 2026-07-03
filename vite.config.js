cbb068bd90895fc424b8804f64562880440dfe33
snomura <you@example.com>
2026-07-04 07:31:31 +0900

    Fix Vite dev server URL for Docker

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        origin: 'http://localhost:5173',
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         react(),
         laravel({
